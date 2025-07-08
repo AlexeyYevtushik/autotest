061788ded71e205e50a58151841b8630763cedfa
AlexeyYevtushik <alexey.yevtushik@gmail.com>
2025-07-08 15:04:55 +0200

    createt tests for performance glitch user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,13 +1,17 @@
 from playwright.sync_api import Page
+from utils.read_config import AppConfiguration
 
 class LoginPage:
     def __init__(self, page: Page):
         self.page = page
+        config = AppConfiguration.get_app_configuration()
+        self.default_navigation_timeout = int(config["DefaultNavigationTimeout"])
+        self.default_timeout = int(config["DefaultTimeout"])
 
     def login(self, username: str, password: str):
-        self.page.fill('input[data-test="username"]', username)
-        self.page.fill('input[data-test="password"]', password)
-        self.page.click('input[data-test="login-button"]')
+        self.page.fill('input[data-test="username"]', username, timeout=self.default_timeout)
+        self.page.fill('input[data-test="password"]', password, timeout=self.default_timeout)
+        self.page.click('input[data-test="login-button"]', timeout=self.default_timeout)
 
     def get_error(self):
         return self.page.locator('[data-test="error"]')
@@ -20,4 +24,3 @@ class LoginPage:
         error_locator = self.page.locator('[data-test="error"]')
         assert error_locator.is_visible(), "Error message is not visible"
         assert error_locator.inner_text() == message, f"Expected error message '{message}', but got '{error_locator.inner_text()}'"
-        
